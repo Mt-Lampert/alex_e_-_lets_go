e360252c67384a2881083cbe9e3f8832d56786a2
Mt-Lampert <perrin.blog@web.de>
2024-05-31 17:35:06 +0200

    w: implementing basic server with net/http 1.22

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a simple home Handler function which writes a byte slice
+// containing "Hello from Snippetbox" as a response body
+func home(w http.ResponseWriter, r *http.Request) {
+	// Write() accepts only []byte as ‘most neutral’ message type
+	w.Write([]byte(`Hello from Snippetbox!`))
+}
 
 func main() {
-	fmt.Println("Hello gophers!")
+	// use the http.NewServeMux() constructor to initialize a new servemux (router),
+	// then register the home() function as handler for the `/` endpoint.
+	mux := http.NewServeMux()
+	mux.HandleFunc(`GET /`, home)
+
+	// Use the http.ListenAndServe() function as web serving unit. It accepts two parameters:
+	//   - the URL (which will be `localhost:3000` here)
+	//   - the router we just created.
+	// If the webserver returns an error, we handle it using log.Fatal() to log the error and exit.
+	// Note that any error returned by http.ListenAndServe() is non-nil!
+	log.Println("starting server at port :3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatalf("Uh oh! %s", err)
+	}
 }
 
 // vim: ts=4 sw=4 fdm=indent
